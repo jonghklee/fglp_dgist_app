fbcdeb5f4e723887791652c9b83a418031e24942
jonghklee <42422167+jonghklee@users.noreply.github.com>
2022-11-12 18:47:30 +0900

    1112 Currency part 1

--- assets/Home/Currency/CurrencyScene.js
+++ assets/Home/Currency/CurrencyScene.js
@@ -1,11 +1,34 @@
-import { View, Text } from 'react-native';
+import React, { useState, useCallback, useRef, useEffect, StyleSheet } from 'react';
+import { View, Text, Button, TouchableOpacity, SafeAreaView } from 'react-native';
 
-export default function BruincardScene({ navigation }) {
-    return(
-        <View>
-            <Text>
-                환율
-            </Text>
-        </View>
+import Feather from 'react-native-vector-icons/Feather';
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+
+import styles from './../HomeSceneStyle';
+
+export default function CurrencyScene({ navigation }) {
+    return (
+        <SafeAreaView style={styles.body}>
+            <View style={{height:178}}>
+                <View style={{ width: 58, height: 60, alignItems: 'flex-end', justifyContent: 'center'}}>
+                    <TouchableOpacity
+                        style={{width: 30, height: 30, alignItems: 'center', justifyContent: "center"}}
+                        onPress={() => {navigation.navigate("HomeInterface");}}
+                        >
+                        <Feather style={{flex:1}} name="chevron-left" size={28} color="black" />
+                    </TouchableOpacity>
+                </View>
+                <Text style={{fontSize: 28, color: "black", fontWeight: "900", paddingLeft: 30}}>환율 계산기</Text>
+            </View>
+            <View style={{backgroundColor:'blue'}}>
+                <Text style={{fontSize: 28, color: "black", fontWeight: "500", paddingLeft: 30}}>환율 계산기</Text>
+                <View style={{backgroundColor:'red', height:30, width:30}}/>
+                <View style={{backgroundColor:'purple', height:30, width:30}}/>
+                <View style={{backgroundColor:'pink', height:30, width:30}}/>
+            </View>
+                <Text style={{fontSize: 26, color: "black", fontWeight: "500"}}>환율: </Text>
+        </SafeAreaView>
     );
 }
+
--- assets/Home/CurrencyButton.js
+++ assets/Home/CurrencyButton.js
@@ -1,9 +1,39 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { View, Text, TouchableHighlight, Animated } from "react-native";
+
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
 
 import styles from "./HomeSceneStyle";
 
 export default function CurrencyButton({ CurrencyRefresh, isShrinked, gotoCurrencyScene }) {
+    const [currencyNum, setCurrencyNum] = useState('데이터를 가져오는 중...')
+    const currencyUrl = 'https://spib.wooribank.com/pib/Dream?withyou=CMCOM0184';
+    const jsCode_getExchangeRate = `
+    setTimeout(() => {
+        const timer = ms => new Promise(res=>setTimeout(res,ms));
+        async function getEx() {
+            let notFinished = true;
+            let exchangeRate = "";
+            let fail_num = 0;
+            let lmt = 50;
+            while (notFinished) {
+                try {
+                    exchangeRate = document.querySelector('#fxprint > table > tbody > tr:nth-child(1) > td:nth-child(3)').innerHTML;
+                    window.ReactNativeWebView.postMessage( exchangeRate );
+                    notFinished = false;
+                } catch (err) {
+                    await timer(100);
+                    fail_num += 1;
+                    if(fail_num > lmt) {
+                        window.ReactNativeWebView.postMessage( "NONE" );
+                    }
+                }
+            }
+        }
+        getEx();
+    }, 100);
+    `
     const textfade = useRef(new Animated.Value(1)).current;
     
     useEffect(() => {
@@ -23,6 +53,12 @@ export default function CurrencyButton({ CurrencyRefresh, isShrinked, gotoCurren
 
     }
 
+    const w2n = async (e) => {
+        await AsyncStorage.setItem('currency', e.nativeEvent.data);
+        setCurrencyNum(e.nativeEvent.data);
+        //setCurrencyNum(AsyncStorage.getItem('currency'));
+    };
+
     return(
         <TouchableHighlight
             style={[styles.currencyform, styles.shadowProp]}
@@ -36,9 +72,18 @@ export default function CurrencyButton({ CurrencyRefresh, isShrinked, gotoCurren
                         현재환율(보낼때)
                     </Text>
                     <Text style={{fontSize: 26, color: 'black', fontWeight: '500'}}>
-                        1337.4
+                        {currencyNum}
                     </Text>
                 </View>
+            <View style={{height:1}}>
+                <WebView
+                  javaScriptEnabled={true}
+                  injectedJavaScript={jsCode_getExchangeRate}
+                  source={{uri: currencyUrl}}
+                  onMessage={w2n}
+                  style={{flex:0}}
+                />
+            </View>
                 <Animated.Text style={{fontSize: 10, color: 'black', fontWeight: '400', opacity: textfade}}>
                     미국시기준 9월 14일(수){'\n'}오후 5:23 업데이트 됨
                 </Animated.Text>
